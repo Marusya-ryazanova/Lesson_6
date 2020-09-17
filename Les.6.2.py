45622a5e238aa48a2b54ba46a94f7cf68b855e01
Marusya-ryazanova <70341111+Marusya-ryazanova@users.noreply.github.com>
2020-09-17 19:34:14 +0300

    Add files via upload

--- Les.6.2.py
+++ Les.6.2.py
@@ -1,10 +1,16 @@
-def my_decorator(function):
-    def wrapper(*args, **kwargs):
-        result = function(*args, **kwargs)
-        return '1 \n 2 \n 3 \n'
-    return wrapper
-    time.sleep(1)
-    @my_decorator
-    def my_decorated_function(input):
-        return input
-    print(time.strftime("Today is %B %d, %Y.", time.localtime()))
+def my_decorator(input_arg):
+    def the_real_decorator(function):
+        def wrapper(*args, **kwargs):
+            result = function(*args, **kwargs)
+            return f'{input_arg}\n{result}\n{input_arg}'
+
+        return wrapper
+
+    return the_real_decorator
+import time
+@my_decorator('-------------')
+def my_decorated_function(input):
+    return input
+
+
+print(time.strftime("Today is %B %d, %Y.", time.localtime()))
